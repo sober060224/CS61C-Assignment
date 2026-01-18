510f4a784cd2c61a7b15cba50ad82d83bf0bd6dc
sober060224 <2680038634@qq.com>
2026-01-18 12:10:33 +0800

    update a little lab02

--- Lab/lab02/bit_ops.c
+++ Lab/lab02/bit_ops.c
@@ -4,24 +4,28 @@
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
-                 unsigned n) {
+                 unsigned n)
+{
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
+    // no sense, because get_bit only returns
     // 0 or 1)
+    x >> n;
+    return x & 1;
     return -1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
+void set_bit(unsigned *x,
              unsigned n,
-             unsigned v) {
+             unsigned v)
+{
     // YOUR CODE HERE
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
+void flip_bit(unsigned *x,
+              unsigned n)
+{
     // YOUR CODE HERE
 }
-
